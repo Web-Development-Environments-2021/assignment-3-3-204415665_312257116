1cf25b223754fd778086a1213334ba80f20027c1
DanielDinshtein <danieldinsh@gmail.com>
2021-06-21 19:00:24 +0300

    before starting unionAgent - league management

--- src/pages/unionAgentPages/unionAgent_leagueManagementPage.vue
+++ src/pages/unionAgentPages/unionAgent_leagueManagementPage.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        
+        <FutureMatchPreview></FutureMatchPreview>
+        <PastMatchPreview></PastMatchPreview>
     </div>
 </template>
 
 
 <script>
+
+import FutureMatchPreview from "../../components/matches/matches_futureMatchPreview"
+import PastMatchPreview from "../../components/matches/matches_pastMatchPreview"
+
 export default {
-    name: "leagueManagement"
+    name: "leagueManagement",
+
+    components: {
+        FutureMatchPreview,
+        PastMatchPreview
+    },
+
+    data(){
+        return {
+            pastMatches: [],
+            futureMatches: [],
+        }
+    },
+
+    // mounted(){
+
+    // }
+
+
 }
 </script>
 

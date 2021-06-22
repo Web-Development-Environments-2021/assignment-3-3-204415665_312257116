9cc0751a7a3eaf58df6c8e957bac2c08d62e0554
DanielDinshtein <danieldinsh@gmail.com>
2021-06-22 03:42:07 +0300

    UnionAgent - Before change mount

--- src/pages/unionAgentPages/unionAgent_leagueManagementPage.vue
+++ src/pages/unionAgentPages/unionAgent_leagueManagementPage.vue
@@ -1,106 +1,124 @@
 <template>
   <div>
-    <b-pagination
-      v-model="future_currentPage"
-      :total-rows="rows"
-      :per-page="future_perPage"
-      aria-controls="future-matches-pagination"
-    ></b-pagination>
-
-    <p class="mt-3">Current Page: {{ future_currentPage }}</p>
-
-    <b-table
-      id="future-matches-table"
-      :items="futureMatches"
-      :per-page="future_perPage"
-      :current-page="future_currentPage"
-      :busy="future_isBusy"
-      small>
-    <template #table-busy>
-        <div class="text-center text-danger my-2">
-            <b-spinner class="align-middle"></b-spinner>
-            <strong>Loading...</strong>
-        </div>
-    </template>
-    </b-table>
+    <div>
+        <h2 class="match-headline"> Future Matches </h2>
+        <b-table
+        class="future-matches-table"
+        :items="futureMatches"
+        :per-page="future_perPage"
+        :current-page="future_currentPage"
+        :busy="future_isBusy"
+        small>
+            <template #table-busy>
+                <div class="text-center text-danger my-2">
+                    <b-spinner class="align-middle"></b-spinner>
+                    <strong>Loading...</strong>
+                </div>
+            </template>
+        </b-table>
+        <b-pagination
+        v-model="future_currentPage"
+        :total-rows="future_rows"
+        :per-page="future_perPage"
+        aria-controls="future-matches-table"
+        class="future-matches-pagination">
+        </b-pagination>
+    </div>
+    <div>
+        <h2 class="match-headline"> Past Matches </h2>
+        <b-table
+        class="past-matches-table"
+        :items="pastMatches"
+        :per-page="past_perPage"
+        :current-page="past_currentPage"
+        :busy="past_isBusy"
+        small>
+            <template #table-busy>
+                <div class="text-center text-danger my-2">
+                    <b-spinner class="align-middle"></b-spinner>
+                    <strong>Loading...</strong>
+                </div>
+            </template>
+        </b-table>
+        <b-pagination
+        v-model="past_currentPage"
+        :total-rows="past_rows"
+        :per-page="past_perPage"
+        aria-controls="past-matches-table"
+        class="past-matches-pagination">
+        </b-pagination>
+    </div>
   </div>
-
-
-    <!-- <div class="container">
-        <h1 class="title">League Management</h1>
-        <h2> Future Matches </h2>
-        <span v-for="g in futureMatches" :key="g.id">
-            <FutureMatchPreview
-                
-                :matchID="g.matchID" 
-                :matchDate="g.matchDate" 
-                :localTeamName="g.localTeamName" 
-                :visitorTeamName="g.visitorTeamName" 
-                :venueName="g.venueName" 
-                :refereeInformation="g.refereeInformation"
-                >
-            </FutureMatchPreview>
-        </span>
-        <h2> Past Matches </h2>
-        <PastMatchPreview
-            v-for="g in pastMatches"
-            :matchID="g.matchID" 
-            :matchDateAndTime="g.matchDateAndTime" 
-            :localTeamName="g.localTeamName" 
-            :visitorTeamName="g.visitorTeamName" 
-            :venueName="g.venueName" 
-            :localTeamScore="g.localTeamScore"
-            :visitorTeamScore="g.visitorTeamScore"
-            :refereeInformation="g.refereeInformation"
-            :eventsLog="g.eventsLog"
-            :key="g.id">
-        </PastMatchPreview>
-    </div> -->
 </template>
 
 
 <script>
-import FutureMatchPreview from "../../components/matches/matches_futureMatchPreview"
-import PastMatchPreview from "../../components/matches/matches_pastMatchPreview"
+
 export default {
     name: "leagueManagement",
-    components: {
-        // FutureMatchPreview,
-        // PastMatchPreview
-    },
+
     data(){
         return {
             pastMatches: [],
             futureMatches: [],
 
-            future_perPage: 10,
+            future_perPage: 5,
             future_currentPage: 1,
-            future_isBusy: true
+            future_isBusy: true,
+
+            past_perPage: 5,
+            past_currentPage: 1,
+            past_isBusy: true,
+
         }
     },
+
+    // TODO: Extract referee + eventLog
 
     computed: {
-        rows(){
+        future_rows(){
             return this.futureMatches.length;
+        },
+        past_rows(){
+            return this.pastMatches.length;
         }
     },
-
     mounted(){
         
         this.future_isBusy = true;
+        this.past_isBusy = true;
         this.pastMatches.push(...this.$root.store.leaguePastMatches);
         this.futureMatches.push(...this.$root.store.leagueFutureMatches);
         console.log("League Management Mounted");
         this.future_isBusy=false;
-    }
+        this.past_isBusy=false;
 
+    }
 }
 </script>
 
-
 <style>
-#future-matches-table {
-  margin: 10px 0 10px;
+.future-matches-table {
+  margin: 50px;
   width: 90%;
+}
+.future-matches-pagination {
+    margin-left: 50%; 
+    margin-right: 50%;
+}
+.match-headline {
+    margin-top: revert;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+}
+.past-matches-table {
+  margin: 50px;
+  width: 90%;
+}
+.past-matches-pagination {
+    margin-left: 50%; 
+    margin-right: 50%;
 }
 </style>

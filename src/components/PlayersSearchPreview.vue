3745947176ddc5a9d45b99bfd8b38cc91b78babb
DanielDinshtein <danieldinsh@gmail.com>
2021-07-10 16:15:36 +0300

    Start - Player Page

--- src/components/PlayersSearchPreview.vue
+++ src/components/PlayersSearchPreview.vue
@@ -11,13 +11,17 @@
     <b-card-group id="div-card" deck>
       <b-card 
         style=""
-        :title="this.player.name"
         :img-src="this.player.image"
         img-alt="Image"
         img-top
         tag="article"
         class="mb-2 my-card"
       >
+      <b-card-title>
+        <router-link :to="`/players/playerDetails/${ this.player.playerID }`" class="player-link-text" >
+            {{ this.player.name }}
+        </router-link>
+      </b-card-title>
       <b-card-text>
           team name:<b>{{this.player.team_name}}</b>
            <br> 
@@ -46,37 +50,47 @@ export default {
 
 <style scoped>
 
-
-  .card{
-    width: 11rem!important;
-    height: 13rem!important;
-    font-size: 10.5px!important;
-    background-color: rgb(252, 151, 85)!important;
-    box-shadow: 4px 5px rgba(99, 36, 0, 0.856);
-    border-radius: 30px;
-    padding: 5px;
-  }
-
-  .card-img-top {
-    width: 55%;
-    height: 45%;
-    object-fit: cover;
-    margin: auto;
-    border-radius: 100px;
-    border-color:rgb(161, 6, 6);
-    background-color: rgb(255, 255, 255)!important;
-
-    border-width:200px ;
-    box-shadow: 1px 1px 1px 1px rgb(2, 2, 2);
-
-    /* background-color: rgba(219, 157, 0, 0.966); */
+.card{
+  width: 11rem!important;
+  height: 13rem!important;
+  font-size: 10.5px!important;
+  background-color: rgb(252, 151, 85)!important;
+  box-shadow: 4px 5px rgba(99, 36, 0, 0.856);
+  border-radius: 30px;
+  padding: 5px;
 }
-  .card-title{
-    font-size: 13px!important;
-    font-weight:bold;
-    text-align: center;
-  }
 
+.card-img-top {
+  width: 55%;
+  height: 45%;
+  object-fit: cover;
+  margin: auto;
+  border-radius: 100px;
+  border-color:rgb(161, 6, 6);
+  background-color: rgb(255, 255, 255)!important;
+
+  border-width:200px ;
+  box-shadow: 1px 1px 1px 1px rgb(2, 2, 2);
+
+  /* background-color: rgba(219, 157, 0, 0.966); */
+}
+
+.card-title{
+  font-size: 13px!important;
+  font-weight:bold;
+  text-align: center;
+}
+
+.player-link-text {
+  font-size: 13px!important;
+  font-weight:bold;
+  text-align: center;
+  color: black;
+}
+
+.player-link-text:hover {
+  color: blue;
+}
 
 </style>
 
--- src/pages/playerDetailsPage.vue
+++ src/pages/playerDetailsPage.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-            
+    <div id="player-page">
+        <h1 class="player-headline"> League Matches </h1>
+        <ol>
+            <li> {{ playerID }} </li>
+            <li> {{ name }} </li>
+            <li> {{ playerID }} </li>
+            <li> {{ team_name }} </li>
+            <li> {{ commonName }} </li>
+            <li> {{ nationality }} </li>
+            <li> {{ birthDate }} </li>
+            <li> {{ birthCountry }} </li>
+            <li> {{ height }} </li>
+            <li> {{ weight }} </li>
+        </ol>
     </div>
 </template>
 
@@ -10,7 +22,7 @@
 export default {
     name: "PlayerDetails",
 
-    data(){
+    data() {
         return {
 
             playerID: "",
@@ -40,12 +52,12 @@ export default {
             this.birthCountry = player.birthCountry;
             this.height = player.height;
             this.weight = player.weight;
-            
+
         }
     },
 
     mounted() {
-        
+    
         this.playerID = this.$route.params.playerID;
 
         var playersInfo = JSON.parse(localStorage.getItem("playersInfo"));
@@ -53,20 +65,37 @@ export default {
         if ( playersInfo != undefined ){
 
             playersInfo.map(( player ) => {
-
                 if ( player.playerShortInfo.playerID == this.playerID ) {
-
+                    this.extractPlayerInfo(player);
                 }
-
             })
         }
     }
-
 }
 
 </script>
 
 
 <style>
-    
+
+#player-page {
+    margin: auto;
+    background-color: #293241e0;
+    border-radius: 10px;
+    max-width: 98%;
+}
+
+.player-headline {
+    margin-top: revert;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    color: rgb(255, 238, 208);
+    font-weight: bold;
+    font-style: oblique;
+    text-decoration: underline;
+}
+
+
 </style>
